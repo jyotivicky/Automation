2b15b94f600e96f3956a15b6db6c2e11d477771b
Jyotiprakash <jyotiqa009@gmail.com>
2018-06-28 17:49:56 +0530

    test2

--- src/main/java/PageClass/ClaimPOP_UP_Test.java
+++ src/main/java/PageClass/ClaimPOP_UP_Test.java
@@ -53,7 +53,7 @@ import CommonClass.HomeFunction;
 		    else
 		      {
 			   System.out.println("Claim Button is Present");
-			   System.out.println("testing done12345566kjrrlkjfrffrfr");
+			   System.out.println("testing done12345566kjrrlkjfrffrfrkkjckjkjlkjclkjdkjdkjlkdlkjelkjdede");
 			   Click_Claim_Button.click();
 			   
 			   System.out.println("Claim Company is Working fine");
